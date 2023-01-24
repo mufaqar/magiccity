4b59b597a92cd2343c9763fe4eca31ab2cf520fd
Mufaqar Islam <mufaqar@gmail.com>
2023-01-24 23:55:56 +0500

    updated

--- functions.php
+++ functions.php
@@ -114,23 +114,23 @@ function email_notification_to_customer( $order_id ) {
 
 
 
-add_action("woocommerce_order_status_changed", "my_awesome_publication_notification");
+//add_action("woocommerce_order_status_changed", "my_awesome_publication_notification");
 
-function my_awesome_publication_notification($order_id, $checkout=null) {
+function my_awesome_publication_notification($order_id) {
    global $woocommerce;
    $order = new WC_Order( $order_id );
-   if($order->status === 'pickup' ) {
+  
       // Create a mailer
       $mailer = $woocommerce->mailer();
 
-      $message_body = __( 'Hello world!!!' );
+      $message_body = __( 'Order is Ready to Pick Up' );
 
       $message = $mailer->wrap_message(
         // Message head and message body.
         sprintf( __( 'Order %s received' ), $order->get_order_number() ), $message_body );
       // Cliente email, email subject and message.
        $mailer->send( $order->billing_email, sprintf( __( 'Order %s received' ), $order->get_order_number() ), $message );
-     }
+    
 
    }
 
